15f0a1cb9cd63620a95b53688d7910b68840c141
sujith758 <p.sujithk@gmail.com>
2019-10-02 00:06:20 +0530

    pysimplestack

---
+++ src/simplestack.py
@@ -0,0 +1,11 @@
+#Simple stack program
+#Creating a list
+list = []
+list.append(1)
+list.append(2)
+list.append(3)
+print(list)
+#Using pop to show last filled element
+last = list.pop(1)
+print(last)
+print(list)
